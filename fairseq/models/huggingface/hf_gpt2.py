1b6a65c5fc5903412606cdcf26ad0473fecd1586
Matthew A. Wright <mwright@berkeley.edu>
2020-10-26 17:07:28 -0700

    Revert "Fix library usage of --user-dir (primarily affects tests) (#1346)"
    This reverts commit c4d322ad9d3e6907e54976289d95c3d9b571a5c3.

--- fairseq/models/huggingface/hf_gpt2.py
+++ fairseq/models/huggingface/hf_gpt2.py
@@ -21,11 +21,10 @@ try:
     # Prepend the transformers submodule to the path, so that
     # it's prioritized over other installations. This allows
     # making local changes in the submodule.
-    hf_path = os.path.join(os.path.dirname(__file__), "transformers", "src")
-    sys.path.insert(0, hf_path)
-    from transformers import GPT2Config, GPT2LMHeadModel
-
-    sys.path.remove(hf_path)
+    sys.path.insert(
+        0, os.path.join(os.path.dirname(__file__), 'transformers', 'src')
+    )
+    from transformers import AutoModel, GPT2Config, GPT2LMHeadModel
     has_hf = True
 except ImportError:
     has_hf = False
@@ -78,7 +77,15 @@ class HuggingFaceGPT2Decoder(FairseqIncrementalDecoder):
     def __init__(self, args, task):
         super().__init__(task.target_dictionary)
 
-        if not has_hf:
+        try:
+            # Prepend the transformers submodule to the path, so that
+            # it's prioritized over other installations. This allows
+            # making local changes in the submodule.
+            sys.path.insert(
+                0, os.path.join(os.path.dirname(__file__), 'transformers', 'src')
+            )
+            from transformers import GPT2Config, GPT2LMHeadModel
+        except ImportError:
             raise ImportError(
                 "\n\nPlease install huggingface/transformers with:"
                 "\n\n  pip install transformers"
